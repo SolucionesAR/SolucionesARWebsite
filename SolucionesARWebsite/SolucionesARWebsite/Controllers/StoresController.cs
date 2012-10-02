3c99c9b624a80dc88881789a15468413da6b4033
calito1307 <ric13qh@gmail.com>
2012-10-01 21:15:14 -0600

    Initial Commits.
    Adding first version with the models definitions, migrations and basic
    controllers.

---
+++ SolucionesARWebsite/SolucionesARWebsite/Controllers/StoresController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SolucionesARWebsite.Models;
+
+namespace SolucionesARWebsite.Controllers
+{
+    public class StoresController : Controller
+    {
+        private DbModel db = new DbModel();
+
+        //
+        // GET: /Stores/
+
+        public ActionResult Index()
+        {
+            var stores = db.Stores.Include(s => s.Company);
+            return View(stores.ToList());
+        }
+
+        //
+        // GET: /Stores/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Store store = db.Stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        //
+        // GET: /Stores/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.CompanyId = new SelectList(db.Companies, "CompanyId", "CompanyName");
+            return View();
+        }
+
+        //
+        // POST: /Stores/Create
+
+        [HttpPost]
+        public ActionResult Create(Store store)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Stores.Add(store);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CompanyId = new SelectList(db.Companies, "CompanyId", "CompanyName", store.CompanyId);
+            return View(store);
+        }
+
+        //
+        // GET: /Stores/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Store store = db.Stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CompanyId = new SelectList(db.Companies, "CompanyId", "CompanyName", store.CompanyId);
+            return View(store);
+        }
+
+        //
+        // POST: /Stores/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Store store)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(store).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CompanyId = new SelectList(db.Companies, "CompanyId", "CompanyName", store.CompanyId);
+            return View(store);
+        }
+
+        //
+        // GET: /Stores/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Store store = db.Stores.Find(id);
+            if (store == null)
+            {
+                return HttpNotFound();
+            }
+            return View(store);
+        }
+
+        //
+        // POST: /Stores/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Store store = db.Stores.Find(id);
+            db.Stores.Remove(store);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
